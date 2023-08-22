38884ca8aebb63a5a7b874a3462fde36b6fbd478
eduardocDev <eduardoc.dev@gmail.com>
2023-08-22 16:44:21 -0300

    This Is a Application using Laravel and Bulma

---
+++ resources/views/quotes.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Quotes</title>
+</head>
+<body>
+   
+    {{-- Hero --}}
+    <section class="hero is-primary">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title is-1">Coding Quotes</h1>
+                <h2 class="subtitle">Like your favorites</h2>
+            </div>
+        </div>
+    </section>
+
+    {{-- colums --}}
+
+    <div class="columns">
+        <div class="column">
+            <div class="card">
+                <div class="card-content">
+                    <h2 class="title">"Acredito que às vezes são as pessoas que ninguém espera nada que fazem as coisas que ninguém consegue imaginar."</h2>
+                    <h3 class="subtitle">Alan Turing</h3>
+                </div>
+                <footer class="card-footer">
+                    <div class="card-footer-item">
+                        <a href="#" class="button is-success">
+                            <i class="icon"></i>
+                        </a>
+                    </div>
+                    <div class="card-footer-item">
+                        <a href="#" class="button is-danger">
+                            <i class="icon"></i>
+                        </a>
+                    </div>
+                    <div class="card-footer-item">
+                        <a href="#" class="button is-info">
+                            <i class="icon"></i>
+                        </a>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <div class="card-content">
+                    <h2 class="title">"O objetivo da ciência é descrever a ordem no caos."</h2>
+                    <h3 class="subtitle"> John von Neumann</h3>
+                </div>
+                <footer class="card-footer">
+                    <div class="card-footer-item">
+                        <a href="#" class="button is-success">
+                            <i class="icon"></i>
+                        </a>
+                    </div>
+                    <div class="card-footer-item">
+                        <a href="#" class="button is-danger">
+                            <i class="icon"></i>
+                        </a>
+                    </div>
+                    <div class="card-footer-item">
+                        <a href="#" class="button is-info">
+                            <i class="icon"></i>
+                        </a>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <div class="card-content">
+                    <h2 class="title">"A única maneira de aprender é fazer."</h2>
+                    <h3 class="subtitle">Steve Jobs</h3>
+                </div>
+                <footer class="card-footer">
+                    <div class="card-footer-item">
+                        <a href="#" class="button is-success">
+                            <i class="icon"></i>
+                        </a>
+                    </div>
+                    <div class="card-footer-item">
+                        <a href="#" class="button is-danger">
+                            <i class="icon"></i>
+                        </a>
+                    </div>
+                    <div class="card-footer-item">
+                        <a href="#" class="button is-info">
+                            <i class="icon"></i>
+                        </a>
+                    </div>
+                </footer>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
